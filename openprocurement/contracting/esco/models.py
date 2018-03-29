a36180b483f76e3f86a5bf031d131478cb9445b8
VDigitall <vitalldh@gmail.com>
2018-03-29 16:04:41 +0300

    Update Contract model and create Milestone model

--- openprocurement/contracting/esco/models.py
+++ openprocurement/contracting/esco/models.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from zope.interface import implementer
 from schematics.transforms import whitelist
-from schematics.types import StringType, FloatType
+from schematics.types import StringType, FloatType, IntType
 from schematics.types.compound import ModelType
 from schematics.types.serializable import serializable
 from openprocurement.contracting.core.models import (
     IContract,
-    Contract as BaseContract
+    Contract as BaseContract, Document as BaseDocument
 )
 from openprocurement.api.models import (
     plain_role, schematics_default_role,
-    Value,
+    Value, Period, Model
 )
 from openprocurement.contracting.core.models import (
     contract_create_role, contract_edit_role,
@@ -23,12 +23,70 @@ class IESCOContract(IContract):
     """ ESCO Contract marker interface """
 
 
+class Document(BaseDocument):
+    """ Contract Document """
+
+    documentOf = StringType(
+        required=True,
+        choices=['tender', 'item', 'lot', 'contract', 'change', 'milestone'],
+        default='contract'
+    )
+
+    def validate_relatedItem(self, data, relatedItem):
+            if not relatedItem and \
+                    data.get('documentOf') in ['item', 'change', 'milestone']:
+                raise ValidationError(u'This field is required.')
+            if relatedItem and isinstance(data['__parent__'], Model):
+                contract = get_contract(data['__parent__'])
+                if data.get('documentOf') == 'change' and \
+                        relatedItem not in [i.id for i in contract.changes]:
+                    raise ValidationError(
+                        u"relatedItem should be one of changes"
+                    )
+                if data.get('documentOf') == 'item' and \
+                        relatedItem not in [i.id for i in contract.items]:
+                    raise ValidationError(
+                        u"relatedItem should be one of items"
+                    )
+                if data.get('documentOf') == 'milestone' and \
+                        relatedItem not in [i.id for i in contract.milesones]:
+                    raise ValidationError(
+                        u"relatedItem should be one of milestones"
+                    )
+
+
+class Milestone(Model):
+    """ Contract Milestone """
+
+    id = MD5Type(required=True, default=lambda: uuid4().hex)
+    date = IsoDateTimeType()
+    dateModified = IsoDateTimeType()
+    description = StringType()
+    period = ModelType(Period)
+    sequenceNumber = IntType(required=True, min_number=1, max_number=16)
+    status = StringType(
+        required=True,
+        choices=['scheduled', 'met', 'notMet', 'partiallyMet'],
+        default='scheduled'
+    )
+    value = ModelType(Value, required=True)
+    amountPaid = ModelType(
+        Value,
+        default={'amount': 0, 'currency': 'UAH', 'valueAddedTaxIncluded': True}
+    )
+    title = StringType()
+
+
 @implementer(IESCOContract)
 class Contract(BaseContract):
     """ ESCO Contract """
 
-    contractType = StringType(choices=['common', 'esco.EU'], default='esco.EU')
+    contractType = StringType(choices=['common', 'esco'], default='esco')
+    fundingKind = StringType(choices=['budget', 'other'], default='other')
+    milesones = ListType(ModelType(Milestone), default=list())
+    minValue = ModelType(Value, required=True)
     NBUdiscountRate = FloatType(required=True, min_value=0, max_value=0.99)
+    noticePublicationDate = IsoDateTimeType()
     value = ModelType(ESCOValue)
 
     class Options:
