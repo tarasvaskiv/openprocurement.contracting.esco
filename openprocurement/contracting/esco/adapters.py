c59d5f010b4cb87337dc63f6cfe7a35acfb6498b
annawzz <annawzz@quintagroup.org>
2018-04-02 10:13:30 +0300

    Use adapters

---
+++ openprocurement/contracting/esco/adapters.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openprocurement.contracting.core.adapters import ContractConfigurator
+
+
+class ContractESCOConfigurator(ContractConfigurator):
+    """ ESCO contract configuration adapter """
+
+    name = "ESCO configurator"
--- openprocurement/contracting/esco/includeme.py
+++ openprocurement/contracting/esco/includeme.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from logging import getLogger
 from pkg_resources import get_distribution
+from pyramid.interfaces import IRequest
+
+from openprocurement.api.interfaces import IContentConfigurator
+from openprocurement.contracting.esco.models import IESCOContract, Contract
+from openprocurement.contracting.esco.adapters import ContractESCOConfigurator
 
 PKG = get_distribution(__package__)
 
@@ -9,6 +14,8 @@ LOGGER = getLogger(PKG.project_name)
 
 def includeme(config):
     LOGGER.info('Init contracting.esco plugin.')
-    from openprocurement.contracting.esco.models import Contract
-    config.add_contractType(Contract)
+    config.add_contract_contractType(Contract)
     config.scan("openprocurement.contracting.esco.views")
+    config.registry.registerAdapter(ContractESCOConfigurator,
+                                    (IESCOContract, IRequest),
+                                    IContentConfigurator)
