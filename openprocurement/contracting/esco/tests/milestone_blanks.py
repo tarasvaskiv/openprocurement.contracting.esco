8a3fb913383e2d338ec7422b67500c19cf431f54
VDigitall <vitalldh@gmail.com>
2018-04-13 09:08:12 +0300

    Added tests with comments

--- openprocurement/contracting/esco/tests/milestone_blanks.py
+++ openprocurement/contracting/esco/tests/milestone_blanks.py
@@ -59,13 +59,13 @@ def get_milestone_by_id(self):
     )
 
 
-def failed_patch_milestones(self):
+def patch_milestones_status_change(self):
     scheduled_milestone_id = self.initial_data['milestones'][2]['id']
     pending_milestone_id = self.initial_data['milestones'][0]['id']
     contract_id = self.contract['id']
     data = {'amountPaid': {'amount': 500000}}
 
-    # Patch scheduled milestone
+    # Not allow change milestone in scheduled status
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, scheduled_milestone_id, self.contract_token),
         {'data': data}, status=403
@@ -84,7 +84,9 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Patch first pending milestone
+    # Patch first pending milestone only amountPaid.amount and check changing only dateModified of milestone
+    start_dateModified = self.initial_data['milestones'][0]['dateModified']
+    start_date = self.initial_data['milestones'][0]['date']
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, pending_milestone_id, self.contract_token),
         {'data': data}
@@ -94,10 +96,14 @@ def failed_patch_milestones(self):
     milestone = response.json['data']
     self.assertEqual(milestone['status'], 'pending')
     self.assertEqual(milestone['amountPaid']['amount'], data['amountPaid']['amount'])
+    self.assertEqual(milestone['date'], start_date)
+    self.assertGreater(milestone['dateModified'], start_dateModified)
 
-    # Patch second time first pending milestone
+    # Patch second time first pending milestone amountPaid.amount and status and check changing date and dateModified
     data['amountPaid']['amount'] = 600000
     data['status'] = 'met'
+    start_date = milestone['date']
+    start_dateModified = milestone['dateModified']
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, pending_milestone_id, self.contract_token),
         {'data': data}
@@ -107,10 +113,14 @@ def failed_patch_milestones(self):
     milestone = response.json['data']
     self.assertEqual(milestone['status'], data['status'])
     self.assertEqual(milestone['amountPaid']['amount'], data['amountPaid']['amount'])
+    self.assertGreater(milestone['date'], start_date)
+    self.assertGreater(milestone['dateModified'], start_dateModified)
 
     current_milestone_sequenceNumber = milestone['sequenceNumber']
+    current_milestone_date = milestone['date']
+    current_milestone_dateModified = milestone['dateModified']
 
-    # Check status next milestone
+    # Check status next milestone and check date and dateModified must be equal to current milestone
     next_milestone_id = self.initial_data['milestones'][1]['id']
     response = self.app.get('/contracts/{}/milestones/{}'.format(contract_id, next_milestone_id))
     self.assertEqual(response.content_type, 'application/json')
@@ -119,9 +129,11 @@ def failed_patch_milestones(self):
     next_milestone_sequenceNumber = milestone['sequenceNumber']
     self.assertEqual(milestone['amountPaid']['amount'], 0)
     self.assertEqual(milestone['status'], 'pending')
+    self.assertEqual(milestone['date'], current_milestone_date)
+    self.assertEqual(milestone['dateModified'], current_milestone_dateModified)
     self.assertGreater(next_milestone_sequenceNumber, current_milestone_sequenceNumber)
 
-    # Patch third time first met milestone
+    # Don't allow change milestone in one of terminated statuses (notMet, met, partiallyMet)
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, pending_milestone_id, self.contract_token),
         {'data': data}, status=403
@@ -139,7 +151,7 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Patch second pending milestone to notMet
+    # Don't allow set notMet status for milestone if amountPaid.amount greater than 0
     data['status'] = 'notMet'
     data['amountPaid']['amount'] = 600000
     response = self.app.patch_json(
@@ -160,7 +172,7 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Patch second pending milestone to partiallyMet
+    # Don't allow set partiallyMet status for milestone if amountPaid.amount is greater than milestone.value.amount
     data['status'] = 'partiallyMet'
     data['amountPaid']['amount'] = 600000
     response = self.app.patch_json(
@@ -182,7 +194,7 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Patch second pending milestone to met
+    # Set milestone to met status with amountPaid.amount greater than value.amount
     data['status'] = 'met'
     data['amountPaid']['amount'] = 600000
     response = self.app.patch_json(
@@ -195,7 +207,8 @@ def failed_patch_milestones(self):
     self.assertEqual(milestone['status'], data['status'])
     self.assertEqual(milestone['amountPaid']['amount'], data['amountPaid']['amount'])
 
-    # Patch third pending milestone to met
+    # Don't allow update milestone amountPaid.amount if sum of all milesones amountPaid.amount
+    # greater than contract.value.amount
     next_milestone_id = self.initial_data['milestones'][2]['id']
     data['status'] = 'met'
     data['amountPaid']['amount'] = 600000
@@ -217,7 +230,7 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Patch third pending milestone to met
+    # Don't allow set met status if amountPaid.amount less than value.amount
     del data['amountPaid']
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, next_milestone_id, self.contract_token),
@@ -238,7 +251,7 @@ def failed_patch_milestones(self):
         }
     )
 
-    # Try change period.endDate
+    # Don't allow change period
     many_days = timedelta(days=2000)
     end_date = (get_now() + many_days).isoformat()
     data = {
@@ -257,7 +270,7 @@ def failed_patch_milestones(self):
     self.assertNotEqual(end_date, milestone['period']['endDate'])
     self.assertEqual(milestone['period'], self.initial_data['milestones'][2]['period'])
 
-    # Try patch milestone.value when not any pending changes
+    # Don't allow change milestone.value.amount if not any pending change of contract
     data = {'value': {'amount': 0}}
     response = self.app.patch_json(
         '/contracts/{}/milestones/{}?acc_token={}'.format(contract_id, next_milestone_id, self.contract_token),
