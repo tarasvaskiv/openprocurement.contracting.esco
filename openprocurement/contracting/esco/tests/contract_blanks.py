958de4bac92aceb825dab9737945f8ae48b07d6d
annawzz <annawzz@quintagroup.org>
2018-04-18 13:19:02 +0300

    Code style fixes

--- openprocurement/contracting/esco/tests/contract_blanks.py
+++ openprocurement/contracting/esco/tests/contract_blanks.py
@@ -188,21 +188,22 @@ def contract_type_check(self):
 
 
 def patch_tender_contract(self):
-    response = self.app.patch_json('/contracts/{}'.format(self.contract['id']), {"data": {"title": "New Title"}}, status=403)
+    response = self.app.patch_json('/contracts/{}'.format(
+        self.contract['id']), {"data": {"title": "New Title"}}, status=403)
     self.assertEqual(response.status, '403 Forbidden')
 
     tender_token = self.initial_data['tender_token']
-    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(self.contract['id'], tender_token),
-                                   {"data": {"title": "New Title"}}, status=403)
+    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
+        self.contract['id'], tender_token), {"data": {"title": "New Title"}}, status=403)
     self.assertEqual(response.status, '403 Forbidden')
 
-    response = self.app.patch_json('/contracts/{}/credentials?acc_token={}'.format(self.contract['id'], tender_token),
-                                   {'data': ''})
+    response = self.app.patch_json('/contracts/{}/credentials?acc_token={}'.format(
+        self.contract['id'], tender_token), {'data': ''})
     self.assertEqual(response.status, '200 OK')
     token = response.json['access']['token']
 
-    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(self.contract['id'], token),
-                                   {"data": {"title": "New Title"}})
+    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
+        self.contract['id'], token), {"data": {"title": "New Title"}})
     self.assertEqual(response.status, '200 OK')
     self.assertEqual(response.json['data']['title'], "New Title")
 
@@ -269,9 +270,10 @@ def patch_tender_contract(self):
 
     custom_period_start_date = get_now().isoformat()
     custom_period_end_date = (get_now() + timedelta(days=3)).isoformat()
-    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(self.contract['id'], token),
-                                   {"data": {"period": {'startDate': custom_period_start_date,
-                                                        'endDate': custom_period_end_date}}})
+    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
+        self.contract['id'], token), {"data": {
+            "period": {'startDate': custom_period_start_date,
+                       'endDate': custom_period_end_date}}})
     self.assertEqual(response.status, '200 OK')
 
     response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
@@ -282,12 +284,12 @@ def patch_tender_contract(self):
     self.assertEqual(response.json['data']['status'], 'terminated')
     self.assertEqual(response.json['data']['terminationDetails'], 'sink')
 
-    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(self.contract['id'], token),
-                                   {"data": {"status": "active"}}, status=403)
+    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
+        self.contract['id'], token), {"data": {"status": "active"}}, status=403)
     self.assertEqual(response.status, '403 Forbidden')
 
-    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(self.contract['id'], token),
-                                   {"data": {"title": "fff"}}, status=403)
+    response = self.app.patch_json('/contracts/{}?acc_token={}'.format(
+        self.contract['id'], token), {"data": {"title": "fff"}}, status=403)
     self.assertEqual(response.status, '403 Forbidden')
 
     response = self.app.patch_json('/contracts/some_id', {"data": {"status": "active"}}, status=404)
@@ -296,7 +298,7 @@ def patch_tender_contract(self):
     self.assertEqual(response.json['status'], 'error')
     self.assertEqual(response.json['errors'], [
         {u'description': u'Not Found', u'location':
-            u'url', u'name': u'contract_id'}
+         u'url', u'name': u'contract_id'}
     ])
 
     response = self.app.get('/contracts/{}'.format(self.contract['id']))
