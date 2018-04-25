5111b73fc9e8c74a2374de10067e15de6ad485b9
annawzz <annawzz@quintagroup.org>
2018-04-25 17:30:55 +0300

    Pending milestone updates in allowed period & tests

--- openprocurement/contracting/esco/tests/milestone.py
+++ openprocurement/contracting/esco/tests/milestone.py
@@ -11,6 +11,11 @@ from openprocurement.contracting.esco.tests.milestone_blanks import (
     patch_milestone,
     patch_milestone_description,
     patch_milestone_title,
+    pending_status_update,
+    scheduled_status_update,
+    met_status_update,
+    notMet_status_update,
+    partiallyMet_status_update,
 )
 
 
@@ -22,6 +27,11 @@ class ContractMilestoneResourceTest(BaseContractWebTest):
     test_listing_milestones = snitch(listing_milestones)
     test_get_milestone_by_id = snitch(get_milestone_by_id)
     test_patch_milestones_status_change = snitch(patch_milestones_status_change)
+    test_pending_status_update = snitch(pending_status_update)
+    test_scheduled_status_update = snitch(scheduled_status_update)
+    test_met_status_update = snitch(met_status_update)
+    test_notMet_status_update = snitch(notMet_status_update)
+    test_partiallyMet_status_update = snitch(partiallyMet_status_update)
     test_patch_milestone = snitch(patch_milestone)
     test_patch_milestone_description = snitch(patch_milestone_description)
     test_patch_milestone_title = snitch(patch_milestone_title)
